cd5f93b896e5a6cf26ffe8755f60f908016742e1
BT_1N3_04 <baishakicodes>
2018-10-04 10:17:25 -0400

    as

---
+++ src/runner.java
@@ -0,0 +1,14 @@
+import sun.applet.Main;
+
+public class runner
+{
+    String[] firstName = {"Alex","Jim", "Billy","Kelly", "Michelle","Justin","Frank", "John", "Michael"};
+    String[] lastName = {"Wu", "Wong", "Kent", "Wayne", "Hoboken","Smith", "Louise", "Kansas"};
+
+    public static  void main(String[] args)
+    {
+        Person teacher = new teacher()
+
+    }
+
+}
