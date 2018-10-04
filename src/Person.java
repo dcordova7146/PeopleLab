4ffcc94baab10e16e66204e69d076a5e1188d151
BT_1N3_06@BKTECH.LOCAL <BT_1N3_06@BKTECH.LOCAL>
2018-10-04 09:36:31 -0400

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public abstract class Person {
+    private String firstName, familyName;
+    public Person(String firstName, String familyName){
+        this.firstName = firstName;
+        this.familyName = familyName;
+    }
+    public abstract String tostring();
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFamilyName(){
+        return familyName;
+    }
+    public boolean equal(Person person){
+        if(this.firstName.equals(person.getFirstName()) && this.familyName.equals(person.getFamilyName())){
+            return true;
+        }
+        else return false;
+    }
+}
