7d4fe81b7de85086181feed51d6cff4b4ae7535e
BT_1N3_04 <jtse7192@bths.edu>
2018-10-05 09:43:58 -0400

    asfas

--- src/runner.java
+++ src/runner.java
@@ -2,12 +2,24 @@ import sun.applet.Main;
 
 public class runner
 {
-    String[] firstName = {"Alex","Jim", "Billy","Kelly", "Michelle","Justin","Frank", "John", "Michael"};
-    String[] lastName = {"Wu", "Wong", "Kent", "Wayne", "Hoboken","Smith", "Louise", "Kansas"};
+    private static String[] firstName = {"Alex","Jim", "Billy","Kelly", "Michelle","Justin","Frank", "John", "Michael"};
+    private static String[] lastName = {"Wu", "Wong", "Kent", "Wayne", "Hoboken","Smith", "Louise", "Kansas"};
 
     public static  void main(String[] args)
     {
-        Person teacher = new teacher()
+        Person Teacher = new Teacher();
+        Person Student = new Student();
+        Person Classroom = new Classroom();
+
+        int forLength = 0;
+        for(forLength = 0; forLength <= firstName.length; forLength++)
+        {
+            System.out.println();
+        }
+        for(forLength = 0; forLength <= lastName.length; forLength++)
+        {
+            System.out.println();
+        }
 
     }
 
