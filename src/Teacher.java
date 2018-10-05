d916543b67a4b60b0d84bc98023ff2e85bf2d2e6
sfaruk3131 <sfaruk3131@bths.edu>
2018-10-05 09:16:30 -0400

    update

---
+++ src/Teacher.java
@@ -0,0 +1,19 @@
+public class Teacher extends Person {
+    private String Subject;
+    private String Title;
+
+    public Teacher(String Subject, String Title)
+    {
+
+    }
+
+    public static String getSubject()
+    {
+
+    }
+
+    public static String toString()
+    {
+
+    }
+}
