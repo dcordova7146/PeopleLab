820f80fa2f914dbfbe0946186c0ae371b20cb322
BT_1N3_07 <sfaruk3131@bths.edu>
2018-10-09 09:39:56 -0400

    final comments

--- src/Teacher.java
+++ src/Teacher.java
@@ -2,18 +2,33 @@ public class Teacher extends Person {
     private String Subject;
     private String Title;
 
+    /**
+     * Constructor that refers to subject and title
+     * @param Subject
+     * @param Title
+     * @param firstName
+     * @param familyName
+     */
     public Teacher(String Subject, String Title, String firstName, String familyName)
     {
         super(firstName, familyName);
         this.Subject = Subject;
         this.Title = Title;
     }
-    
+
+    /**
+     * gets the subject
+     * @return
+     */
     public String getSubject()
     {
         return Subject;
     }
 
+    /**
+     * concatenated the title and family name
+     * @return
+     */
     public String toString()
     {
         return this.Title + "." +  " " + this.getFamilyName();
